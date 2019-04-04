8833a2dc897a3f284c170b037d1a7610a8563a5c
Enrique Mora <enriquebeta7@gmail.com>
2019-04-04 19:54:53 -0300

    Implementado un refactor en el codigo.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Footer.css";
 
-class Footer extends Component {
-  state = {};
-  render() {
-    return (
-      <footer>
-        <h4>Todos los derechos reservador por Alfredo Jose</h4>
-      </footer>
-    );
-  }
-}
+const Footer = props => (
+  <footer>
+    <h4>Todos los derechos reservador por Alfredo Jose</h4>
+  </footer>
+);
 
 export default Footer;
