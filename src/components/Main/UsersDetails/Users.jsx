589dd7e2bc48fa7ae1058900310149489470087f
fredojbg <alfredojose1022@gmail.com>
2019-04-05 00:55:51 -0400

    Mejorando Diseño

--- src/components/Main/UsersDetails/Users.jsx
+++ src/components/Main/UsersDetails/Users.jsx
@@ -33,9 +33,11 @@ class Users extends Component {
   };
 
   handleAddUsers = e => {
+    alert("Exito, Click en Cerrar");
     this.setState({
       data: [
-        ...this.state.data, {
+        ...this.state.data,
+        {
           ...this.state.datas
         }
       ]
@@ -45,55 +47,63 @@ class Users extends Component {
     //Definicion de Const para el buscador
     const { filter, data } = this.state;
     const lowercasedFilter = filter.toLowerCase();
-    const filteredData = data.filter(item => Object.values(item).some(value =>
-      value.toLowerCase().includes(lowercasedFilter)
-    ));
+    const filteredData = data.filter(item =>
+      Object.values(item).some(value =>
+        value.toLowerCase().includes(lowercasedFilter)
+      )
+    );
 
     return (
       //Html del Buscador
       <div>
-        <div className="form">
-          <input 
-            type="text" 
-            name="nombre" 
-            onChange={this.handleAddForm} 
-            placeholder="Nombre" 
-          />
+        <div className="modal" id="agregarme">
+          <div className="form">
+          <h1>Ingresa tus datos</h1>
+            <input
+              type="text"
+              name="nombre"
+              onChange={this.handleAddForm}
+              placeholder="Nombre"
+            />
 
-          <input 
-            type="text" 
-            name="apellido" 
-            onChange={this.handleAddForm} 
-            placeholder="Apellido"
-          />
+            <input
+              type="text"
+              name="apellido"
+              onChange={this.handleAddForm}
+              placeholder="Apellido"
+            />
 
-          <input 
-            type="text" 
-            name="phone" 
-            onChange={this.handleAddForm} 
-            placeholder="Numero de Telefono" 
-          />
+            <input
+              type="text"
+              name="phone"
+              onChange={this.handleAddForm}
+              placeholder="Numero de Telefono"
+            />
 
-          <input 
-            type="text" 
-            name="pais" 
-            onChange={this.handleAddForm} 
-            placeholder="Tu Pais" 
-          />
+            <input
+              type="text"
+              name="pais"
+              onChange={this.handleAddForm}
+              placeholder="Tu Pais"
+            />
 
-          <input 
-            type="text" 
-            name="inst" 
-            onChange={this.handleAddForm} 
-            placeholder="Que instrumento tocas" 
-          />
+            <input
+              type="text"
+              name="inst"
+              onChange={this.handleAddForm}
+              placeholder="Que instrumento tocas"
+            />
 
-          <button onClick={this.handleAddUsers} className="btn blue btn-form">
-            Agregar
-          </button>
+            <button onClick={this.handleAddUsers} className="btn btn-form">
+              Agregar
+            </button>
+            <a href="#" className="btn blue btn-close">
+            Cerrar
+          </a>
+          </div>
         </div>
-
         <div className="buscador">
+        <h1>¡Busca un musico!</h1>
           <input
             type="search"
             className="bar-buscador"
@@ -103,12 +113,14 @@ class Users extends Component {
             onChange={this.handleChange}
           />
 
-          <button className="btn blue">Buscar</button>
+          <a href="#agregarme" className="btn btn-add">
+            Agregarme
+          </a>
         </div>
 
         {/* Html + Map de Users con Buscador para los usuarios */}
 
-        {filteredData.map((usersDetail) => (
+        {filteredData.map(usersDetail => (
           <div key={usersDetail.id}>
             <article>
               <div className="img">
